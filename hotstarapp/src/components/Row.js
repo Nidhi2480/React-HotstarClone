664bbe259755ae3f8901e532ec6c76831a2ee9f2
nidhi2480 <nidhin1089@gmail.com>
2024-04-24 18:29:10 +0530

    logosize(change)

--- hotstarapp/src/components/Row.js
+++ hotstarapp/src/components/Row.js
@@ -12,7 +12,7 @@ function Row({ title }) {
     const handleMouseEnter = (navname) => {
     timeoutId = setTimeout(() => {
         setHovered(true)
-      }, 1000);
+      }, 500);
       
       setactiveImage(navname)
     }
@@ -24,10 +24,10 @@ function Row({ title }) {
       <div className='row-movies'>
       <h2>{title}</h2>
       <div className='row-contents'>
-        <img src={movielogo1} className={`mov1 ${isHovered && activeimg === 'mov1' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov1')} onMouseLeave={()=>handleMouseLeave} alt="posters"/>
-        <img src={movielogo2}  className={`mov2 ${isHovered && activeimg === 'mov2' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov2')} onMouseLeave={()=>handleMouseLeave} alt="posters"/>
-        <img src={movielogo3}  className={`mov3 ${isHovered && activeimg === 'mov3' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov3')} onMouseLeave={()=>handleMouseLeave} alt="posters"/>
-        <img src={movielogo4}  className={`mov4 ${isHovered && activeimg === 'mov4' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov4')} onMouseLeave={()=>handleMouseLeave} alt="posters"/>
+        <img src={movielogo1} className={`mov1 ${isHovered && activeimg === 'mov1' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov1')} onMouseLeave={()=>handleMouseLeave()} alt="posters"/>
+        <img src={movielogo2}  className={`mov2 ${isHovered && activeimg === 'mov2' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov2')} onMouseLeave={()=>handleMouseLeave()} alt="posters"/>
+        <img src={movielogo3}  className={`mov3 ${isHovered && activeimg === 'mov3' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov3')} onMouseLeave={()=>handleMouseLeave()} alt="posters"/>
+        <img src={movielogo4}  className={`mov4 ${isHovered && activeimg === 'mov4' ? 'active': ''}`} onMouseEnter={()=> handleMouseEnter('mov4')} onMouseLeave={()=>handleMouseLeave()} alt="posters"/>
       </div></div>
      
     );
