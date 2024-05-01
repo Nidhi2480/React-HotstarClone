cdaa2cb47846d078f96c2a8936860219dd2b1aee
nidhi2480 <nidhin1089@gmail.com>
2024-05-01 23:10:25 +0530

    movielist(7 at a time)

--- hotstarapp/src/components/movielistrow/Row.jsx
+++ hotstarapp/src/components/movielistrow/Row.jsx
@@ -5,9 +5,10 @@ import './rowstyle.css';
 export default function App({ rowName, apiUrl, smallRow }) {
   const [data, setData] = useState([]);
   const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
-
-  const [isNavHovered, setNavHovered] = useState(false);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(6); 
   const [isCardHovered, setCardHovered] = useState(false);
+  const [isNavHovered, setNavHovered] = useState(false);
   const rowContentRef = useRef(null);
 
   useEffect(() => {
@@ -43,24 +44,30 @@ export default function App({ rowName, apiUrl, smallRow }) {
   };
 
   const handleScrollRight = () => {
-    rowContentRef.current.scrollLeft += 100; 
+    if (endIndex < data.length - 1) {
+      setStartIndex(startIndex + 7);
+      setEndIndex(endIndex + 7);
+    }
   };
 
   const handleScrollLeft = () => {
-    rowContentRef.current.scrollLeft -= 100; 
+    if (startIndex >= 7) {
+      setStartIndex(startIndex - 7);
+      setEndIndex(endIndex - 7);
+    }
   };
 
   return (
     <>
       <h2 style={{ color: 'white', marginLeft: `19vb`, fontSize: `20px`, fontWeight: `bold` }}>{rowName}</h2>
       <div className="row-movies" onMouseEnter={handleNavMouseEnter} onMouseLeave={handleNavMouseLeave}>
-        <div className={`row-contents${isCardHovered ? ' hide-x' : ''}`} ref={rowContentRef}>
-          {data.map((movie, index) => (
+        <div className={`row-contents${isCardHovered ? ' hide-x' : ''}`}  ref={rowContentRef}>
+          {data.slice(startIndex, endIndex + 1).map((movie, index) => (
             <Card
               key={index}
               movie={movie}
               smallRow={smallRow}
-              onMouseEnter={handleMouseEnter}
+              onMouseEnter={() => handleMouseEnter(movie.id)}
               onMouseLeave={handleMouseLeave}
               isHovered={movie.id === hoveredCardIndex}
             />
