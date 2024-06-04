d0addc174702cf2ff344bf880963a19804f739ba
nidhi2480 <nidhin1089@gmail.com>
2024-06-04 18:18:14 +0530

    volume button change

--- hotstarapp/src/components/bannerdesc/TrailerDesc.jsx
+++ hotstarapp/src/components/bannerdesc/TrailerDesc.jsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import Sideimages from '../bannersideimg/Sideimages';
-import { DefaultMov } from '../../data/Default';
-import Mute from '../../assets/icons/mute.svg';
-import unMute from '../../assets/icons/volumeup.svg';
-import './trailerdesc.css';
-function TrailerDesc({changebanner,movie,handleUnmuteClick,isMuted}) {
-    function handleClick(){
-        handleUnmuteClick()
-    }
+import React from "react";
+import Sideimages from "../bannersideimg/Sideimages";
+import { DefaultMov } from "../../data/Default";
+import unMute from "../../assets/icons/mute.svg";
+import Mute from "../../assets/icons/volumeup.svg";
+import "./trailerdesc.css";
+function TrailerDesc({ changebanner, movie, handleUnmuteClick, isMuted }) {
+  function handleClick() {
+    handleUnmuteClick();
+  }
   return (
     <div className="information">
-    <img className="movieposter" src={movie.posterURL?movie.posterURL:movie.logo} alt="movieposter" />
-    <div className="details">
-        <p>{movie.posterURL?"2017":movie.genre.year}<span className="dot"></span> {movie.posterURL?"2h 15m":movie.genre.length}<span className="dot"></span>{movie.posterURL?"4 languages":movie.genre.languages} <span className="dot"></span> <button>{movie.posterURL?"U/A 13+":movie.genre.ua}</button></p>
-    </div>
-    <div className="description">
-        <p>{movie.posterURL?`${movie.title}${DefaultMov.desc}`:movie.description} </p>
-    </div>
-    <div className="category">
+      <img
+        className="movieposter"
+        src={movie.posterURL ? movie.posterURL : movie.logo}
+        alt="movieposter"
+      />
+      <div className="details">
+        <p>
+          {movie.posterURL ? "2017" : movie.genre.year}
+          <span className="dot"></span>{" "}
+          {movie.posterURL ? "2h 15m" : movie.genre.length}
+          <span className="dot"></span>
+          {movie.posterURL ? "4 languages" : movie.genre.languages}{" "}
+          <span className="dot"></span>{" "}
+          <button>{movie.posterURL ? "U/A 13+" : movie.genre.ua}</button>
+        </p>
+      </div>
+      <div className="description">
+        <p>
+          {movie.posterURL
+            ? `${movie.title}${DefaultMov.desc}`
+            : movie.description}{" "}
+        </p>
+      </div>
+      <div className="category">
         <p>{DefaultMov.category}</p>
-        {isMuted?<img src={unMute} alt="volume"  onClick={handleClick} />:<img src={Mute} alt="volume"  onClick={handleClick} />}
-    </div>
-    <div className="row1">
-
+        {isMuted ? (
+          <img src={unMute} alt="volume" onClick={handleClick} />
+        ) : (
+          <img src={Mute} alt="volume" onClick={handleClick} />
+        )}
+      </div>
+      <div className="row1">
         <div className="buttons">
-            <button className='subscribe'><span className="triangle"></span><span className='btn-text'> Subscribe to Watch</span></button>
-            <button className='add'> + </button></div>
-            {!movie.posterURL && <Sideimages changeBanner={changebanner}/>}
-        
-        
+          <button className="subscribe">
+            <span className="triangle"></span>
+            <span className="btn-text"> Subscribe to Watch</span>
+          </button>
+          <button className="add"> + </button>
+        </div>
+        {!movie.posterURL && <Sideimages changeBanner={changebanner} />}
+      </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default TrailerDesc
+export default TrailerDesc;
